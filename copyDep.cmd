55c632418a695275f70fa6cc1f001c4ca05547cf
Fifflay <finlaywilkinson677@gmail.com>
2023-04-14 12:12:54 +0200

    Add support for Deployment

---
+++ copyDep.cmd
@@ -0,0 +1,4 @@
+C:\Qt\6.5.0\mingw_64\bin\windeployqt.exe "FinanceAnalytics.exe" --no-translations
+copy C:\Qt\6.5.0\mingw_64\bin\libgcc_s_seh-1.dll .
+copy "C:\Qt\6.5.0\mingw_64\bin\libstdc++-6.dll" .
+copy C:\Qt\6.5.0\mingw_64\bin\libwinpthread-1.dll .
